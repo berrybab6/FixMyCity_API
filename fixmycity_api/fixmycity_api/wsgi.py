82ca205129789e9860253b2e512fe754fcc99a5f
berrybab6 <bedatuassefa@gmail.com>
2022-03-15 06:56:16 +0300

    remove whitenoise.django

--- fixmycity_api/fixmycity_api/wsgi.py
+++ fixmycity_api/fixmycity_api/wsgi.py
@@ -14,8 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fixmycity_api.settings')
 
 # from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
