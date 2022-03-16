b4077978f2873244ff4f6139af2a189439d531d4
berrybab6 <bedatuassefa@gmail.com>
2022-03-16 22:40:16 +0300

    comment contrib.admin

--- fixmycity_api/fixmycity_api/settings.py
+++ fixmycity_api/fixmycity_api/settings.py
@@ -98,9 +98,11 @@ DATABASES = {
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CORS_ALLOWED_ORIGINS = [
     "https://fixmycity-24.herokuapp.com",
+    "https://fixmycity5-24.herokuapp.com",
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://fixmycity-24.herokuapp.com',
+    "https://fixmycity5-24.herokuapp.com",
 ]
 
 CORS_ALLOW_METHODS = [
