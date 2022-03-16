33007bbe2f35b12f2eafef24ff77fe09e32371be
berrybab6 <bedatuassefa@gmail.com>
2022-03-16 16:30:04 +0300

    comment contrib.admin

--- fixmycity_api/accounts/admin.py
+++ fixmycity_api/accounts/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
-# from .models import SuperAdmin
+from django.contrib import admin
+from .models import SuperAdmin
 # # Register your models here.
-# admin.site.register(SuperAdmin)
+admin.site.register(SuperAdmin)
