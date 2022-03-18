30f549852d02e14937a1a76ba2eb0d4002fc89ae
berrybab6 <bedatuassefa@gmail.com>
2022-03-18 19:03:59 +0300

    update settings.py

--- fixmycity_api/fixmycity_api/settings.py
+++ fixmycity_api/fixmycity_api/settings.py
@@ -96,19 +96,28 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'fixmycity_api.wsgi.application'
-AUTH_USER_MODEL = 'accounts.SuperAdmin'
+AUTH_USER_MODEL = 'accounts.User'
 
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fixmycity',
+        'USER': 'postgres',
+        'PASSWORD': 'new_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CORS_ALLOWED_ORIGINS = [
     "https://fixmycity-24.herokuapp.com",
