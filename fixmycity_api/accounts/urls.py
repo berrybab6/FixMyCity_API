3b499d55373f4e51b4e51a3207db210b9b9371ee
berrybab6 <bedatuassefa@gmail.com>
2022-03-19 01:32:26 +0300

    add sector path

--- fixmycity_api/accounts/urls.py
+++ fixmycity_api/accounts/urls.py
@@ -1,4 +1,5 @@
 # from django.contrib import admin
+from django import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,17 +7,26 @@ from django.conf.urls.static import static
 # # from graphene_file_upload.django import FileUploadGraphQLView
 
 from django.urls import include
-from .views import EditProfile, LoginView, RegisterView, LoginSerializer, TestView
+from .views import EditProfile, LoginView, RegisterView, LoginSerializer, TestView,SectorView
 #
+from rest_framework import routers
+app_name = 'accounts'
+router = routers.DefaultRouter()
+router.register('',SectorView,basename='Sector')
+# router.register('login',LoginView,basename='User')
+
 urlpatterns = [
     path('register/', RegisterView.as_view()),
+    path('sector/',include(router.urls)),
+
     path('login/', LoginView.as_view()),
     path('api/', include('rest_framework.urls')),
-
+    # path('sector/',SectorView.as_view())
 #     path('test/', TestView.as_view()),
 #     path('edit/<int:id>/',EditProfile.as_view()),
 ]
 
+# urlpatterns+= router.urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
