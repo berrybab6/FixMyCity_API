20737fe7b97067be2328c1a73fd387b25b9a2d8e
berrybab6 <bedatuassefa@gmail.com>
2022-03-19 01:31:38 +0300

    update accounts serializer.py

--- fixmycity_api/accounts/serializers.py
+++ fixmycity_api/accounts/serializers.py
@@ -1,6 +1,6 @@
 from numpy import full
 from rest_framework import serializers, fields
-from .models import SectorAdmin, User
+from .models import Sector, SectorAdmin, User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,15 +9,6 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
         read_only_fields = ['id']
     
-    # def create(self, validated_data):
-    #     if validated_data.get('image'):
-    #         user = User(username= validated_data['username'], full_name= validated_data['full_name'], image= validated_data['image'] )
-    #     else:
-    #         user = User(username= validated_data['username'], full_name = validated_data['full_name'])
-
-    #     user.set_password(validated_data['password'])
-    #     user.save()
-    #     return user
 class SectorAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = SectorAdmin
@@ -28,3 +19,13 @@ class SectorAdminSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, write_only=True)
     username = serializers.CharField(required=True)
+
+    # def create(self, validated_data):
+        # return User.objects.create(**validated_data)
+
+class SectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = "__all__"
+        # extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = ['id']    
